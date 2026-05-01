0431bde949fe3b053becb0c4b82907ce3ccbaa94
dreamforSh <3856889129@qq.com>
2026-05-01 15:32:08 +0800

    forge fix

--- forge/src/main/java/com/github/recipeexport/RecipeExport.java
+++ forge/src/main/java/com/github/recipeexport/RecipeExport.java
@@ -3,7 +3,7 @@ package com.github.recipeexport;
 import com.github.recipeexport.dumper.impl.DumpRecipeCommand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
@@ -14,6 +14,7 @@ public class RecipeExport {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    @SuppressWarnings("unused")
     @SubscribeEvent
     public void onRegisterCommands(RegisterCommandsEvent event) {
         DumpRecipeCommand.register(event.getDispatcher());
